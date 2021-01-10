6a60f50142f26993cf42da3dd3c36cfe747993fa
Sai Priya <saipriyak1998@gmail.com>
2021-01-10 07:58:34 +0530

    changes done

--- js/Game.js
+++ js/Game.js
@@ -29,13 +29,13 @@ class Game {
       form.display();
     }
 
-    car1 = createSprite(100,200);
+    car1 = createSprite(-10,100);
     car1.addImage("car1",car1_img);
-    car2 = createSprite(300,200);
+    car2 = createSprite(-10,300);
     car2.addImage("car2",car2_img);
-    car3 = createSprite(500,200);
+    car3 = createSprite(-10,500);
     car3.addImage("car3",car3_img);
-    car4 = createSprite(700,200);
+    car4 = createSprite(-10,700);
     car4.addImage("car4",car4_img);
     cars = [car1, car2, car3, car4];
   }
@@ -48,7 +48,7 @@ class Game {
     
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
-      image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+      image(track, 0,-displayHeight*4,displayWidth, displayHeight*10);
       
       //var display_position = 100;
       
@@ -56,17 +56,17 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
-      var y;
+      var x ;
+      var y=175;;
 
       for(var plr in allPlayers){
         //add 1 to the index for every loop
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = displayWidth - allPlayers[plr].distance;
         //use data form the database to display the cars in y direction
-        y = displayHeight - allPlayers[plr].distance;
+        y = y+200;
         cars[index-1].x = x;
         cars[index-1].y = y;
        // console.log(index, player.index)
